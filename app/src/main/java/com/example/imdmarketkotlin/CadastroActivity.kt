8fe6ecaaea316274db927f824859d67a3e11688a
viniciusvilelaa <nino.bez@hotmail.com>
2024-12-02 15:43:42 -0300

    Ajuste dos layouts

--- app/src/main/java/com/example/imdmarketkotlin/CadastroActivity.kt
+++ app/src/main/java/com/example/imdmarketkotlin/CadastroActivity.kt
@@ -1,6 +1,7 @@
 package com.example.imdmarketkotlin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imdmarketkotlin.databinding.ActivityCadastroBinding
 
@@ -13,7 +14,26 @@ class CadastroActivity : AppCompatActivity() {
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var listaProdutos = mutableListOf<Produto>()
 
+        binding.btnSalvar.setOnClickListener {
+
+            var codigoTemp = binding.edCodigo.text.toString()
+            var nomeTemp = binding.edNome.text.toString()
+            var descTemp = binding.edDesc.text.toString()
+            var estoqueTemp = binding.edEstoque.text.toString()
+
+
+            if (codigoTemp.isNotEmpty() && nomeTemp.isNotEmpty() && descTemp.isNotEmpty() && estoqueTemp.isNotEmpty()){
+                listaProdutos.add(Produto(codigoTemp,nomeTemp,descTemp,estoqueTemp.toInt()))
+                Toast.makeText(this, "Produto salvo!", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_LONG).show()
+            }
+
+
+
+        }
 
 
     }
